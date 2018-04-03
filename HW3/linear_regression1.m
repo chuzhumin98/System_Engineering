e3f610e593a4b68269a3eaf8f17b02c5893e9124
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-03 19:06:55 +0800

    add CI

--- HW3/linear_regression1.m
+++ HW3/linear_regression1.m
@@ -1,6 +1,6 @@
 %% 得到的最终函数形式为y = ahat + bhat*x
 
-function [ahat, bhat, F, Falpha] = linear_regression1(data, alpha)
+function [ahat, bhat, F, Falpha, rsquare, CI] = linear_regression1(data, alpha)
     %% 计算出参数a,b的估计值ahat和bhat
     Xnorm = data(:,2)-mean(data(:,2));
     Ynorm = data(:,1)-mean(data(:,1));
@@ -13,11 +13,15 @@ function [ahat, bhat, F, Falpha] = linear_regression1(data, alpha)
     Yhat = ahat + bhat*data(:,2);
     ESS = (Yhat - mean(data(:,1)))'* (Yhat - mean(data(:,1))); %解释平方和
     RSS = (data(:,1) - Yhat)' * (data(:,1) - Yhat); %剩余平方和
+    rsquare = ESS/(ESS+RSS);
     F = ESS * (N-2) / RSS; %计算F值
     if (F > Falpha)
         sprintf('我们有%5.3f的信心拒绝原假设，认为x和y之间满足线性关系', 1-alpha)
-    else
-        sprintf('我们没有足够的信心拒绝原假设')
+        %% 计算置信区间
+        S = sqrt(RSS/(N-2));
+        z = norminv(1-alpha/2,0,1);
+        CI(1) = -S*z; %置信区间上下界的变化值
+        CI(2) = S*z;
         %% 绘制相关图像
         Xmin = min(data(:,2)); %所给数据中的横坐标极大极小值
         Xmax = max(data(:,2));
@@ -30,5 +34,7 @@ function [ahat, bhat, F, Falpha] = linear_regression1(data, alpha)
         plot(data(:,2), data(:,1),'r.','MarkerSize',15)
         legend('拟合直线','数据点','Location','NorthEast')
         saveas(gcf, '一维线性拟合效果图.png')
-        end
+    else
+        sprintf('我们没有足够的信心拒绝原假设')
+    end
 end
--- HW3/main.m
+++ HW3/main.m
@@ -1,4 +1,4 @@
 clear all
 [myData] = textread('data.txt'); %导入数据
 alpha = 0.05;
-[ahat, bhat, F, Falpha] = linear_regression1(myData, alpha);
+[ahat, bhat, F, Falpha, rsquare, CI] = linear_regression1(myData, alpha);
