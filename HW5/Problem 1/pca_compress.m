93625c4a992650122dfa7fe5d269020b2b4e970c
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-27 23:51:55 +0800

    start for pca

---
+++ HW5/Problem 1/pca_compress.m
@@ -0,0 +1,11 @@
+%% PCA的压缩函数，实现对于数据的PCA降维压缩
+%  data: N*n 输入数据矩阵
+%  rerr: 相对误差限
+%  pcs:  n*m，每一列为一个主成分
+%  cprs_data: N*m，压缩后的数据
+%  cprs_c: 2*m，压缩的常数，第一行为均值，第二列为方差
+
+function [pcs, cprs_data, cprs_c] = pca_compress(data, rerr)
+    
+
+end
