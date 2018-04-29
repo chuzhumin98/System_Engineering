5a2d04d28c5d44a233fbaf2f84ea7984ea4b310c
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-29 20:35:06 +0800

    complete the core code for k-means

--- HW6/kmeans_clustering.m
+++ HW6/kmeans_clustering.m
@@ -5,6 +5,37 @@ function label = kmeans_clustering(data, num)
     N = length(data); %数据点的个数
     randIndex = ceil(rand(3*num,1)*N);
     randPoints = data(randIndex,:); %随机挑选的3*num个点作为候选聚类中心
-    
-    label = [];
+    clusters = randPoints(1,:); %第一个聚类中心点(num*n)
+    for k=2:num %依次找出其他初始的聚类中心
+        distances = []; %记录距离，采用二范数
+        for i=1:num*3
+            mydist = [];
+            for j=1:k-1
+                mydist = [mydist; (clusters(j,:)-randPoints(i,:))*(clusters(j,:)-randPoints(i,:))'];
+            end
+            distances = [distances; min(mydist)]; %比较的是到各个簇心的最短距离
+        end
+        [maxValue, maxIndex] = max(distances);
+        clusters = [clusters; randPoints(maxIndex,:)];
+    end
+    label = zeros(1,N); %初始化标签
+    %% 进行K-means聚类迭代阶段
+    while (true) 
+        %% 对样本重新打标签阶段
+        for i=1:N
+            delta = repmat(data(i,:),num,1)-clusters;
+            [minValue, minIndex] = min(sum(delta .* delta,2));
+            labelnew(i) = minIndex;
+        end
+        chabie = sum(abs(labelnew-label)); %计算总的label差别大小，只有为0时才停止迭代
+        if (chabie == 0)
+             break
+        end
+        label = labelnew;
+        %% 根据样本标签重新确定聚类中心阶段
+        for i=1:num
+            thisClusterData = data(label(:)==i,:);
+            clusters(i,:) = mean(thisClusterData);
+        end
+    end
 end
--- HW6/main.m
+++ HW6/main.m
@@ -6,4 +6,18 @@ load data.mat
 % ylabel('feature 2')
 % title('data samples distribution')
 % saveas(gcf, 'img/samples distribution.png')
-label = kmeans_clustering(data, 2);
+color = ['m.';'c.';'r.';'g.';'b.';'k.';'y.'];
+for i=2:7
+    if (i ~= 2)
+        figure
+    end
+    label = kmeans_clustering(data, i);
+    for j = 1:i
+        plot(data(label(:)==j,1), data(label(:)==j,2),color(j,:))
+        hold on
+    end
+    xlabel('feature 1')
+    ylabel('feature 2')
+    title(strcat('k-means with k=',num2str(i)))
+    saveas(gcf, strcat('img/k-means with k=',num2str(i),'.png'))
+end
