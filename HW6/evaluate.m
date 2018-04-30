3b488c6fceb54e390b939c42751a49c7c299234c
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-30 12:23:12 +0800

    evaluate time vs size

--- HW6/evaluate.m
+++ HW6/evaluate.m
@@ -1,5 +1,6 @@
 clear all
 load data.mat
+%% 分析聚类数目与目标函数大小的关系
 for i=2:10
     label = kmeans_clustering(data, i);
     thisJ = 0; %该聚类数下的目标函数
@@ -18,3 +19,28 @@ ylabel('J(w,y)')
 title('J(w,y) vs k')
 box on
 saveas(gcf, 'img/J(w,k)_vs_k.png')
+figure
+%% 分析不同的数据规模下程序耗时的变化
+size = [100,200,400,800,1600,3000]; %不同规模的数据点
+ks = [3,5,7]; %同时测试不同k的变化关系
+for i=1:length(size)
+    shuffleOrder = randperm(length(data));
+    thisSizeData = data(shuffleOrder(1:size(i)),:);
+    for j=1:length(ks)
+        tic
+        label = kmeans_clustering(thisSizeData, ks(j));
+        t(j,i) = toc;
+    end
+end
+plot(1:6,t(1,:),'r-^','LineWidth',1.5,'MarkerSize',5)
+hold on
+plot(1:6,t(2,:),'b-.d','LineWidth',1.5,'MarkerSize',5)
+plot(1:6,t(3,:),'k--o','LineWidth',1.5,'MarkerSize',5)
+set(gca, 'xTick', 1:6);  
+set(gca,'XTickLabel',{'100','200','400','800','1600','3000'})  
+xlabel('data size')
+ylabel('cost time')
+title('time vs size')
+legend('k=3','k=5','k=7','Location','NorthWest')
+box on; grid on
+saveas(gcf, 'time-size.png')
