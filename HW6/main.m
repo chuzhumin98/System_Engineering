7423d4b554027da4681ccef15cb07458c7ac2a42
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-30 10:35:05 +0800

    figure for different k

--- HW6/main.m
+++ HW6/main.m
@@ -8,16 +8,17 @@ load data.mat
 % saveas(gcf, 'img/samples distribution.png')
 color = ['m.';'c.';'r.';'g.';'b.';'k.';'y.'];
 for i=2:7
-    if (i ~= 2)
-        figure
-    end
     label = kmeans_clustering(data, i);
-    for j = 1:i
-        plot(data(label(:)==j,1), data(label(:)==j,2),color(j,:))
-        hold on
-    end
-    xlabel('feature 1')
-    ylabel('feature 2')
-    title(strcat('k-means with k=',num2str(i)))
-    saveas(gcf, strcat('img/k-means with k=',num2str(i),'.png'))
+    
+%     if (i ~= 2)
+%         figure
+%     end  
+%     for j = 1:i
+%         plot(data(label(:)==j,1), data(label(:)==j,2),color(j,:))
+%         hold on
+%     end
+%     xlabel('feature 1')
+%     ylabel('feature 2')
+%     title(strcat('k-means with k=',num2str(i)))
+%     saveas(gcf, strcat('img/k-means with k=',num2str(i),'_3.png'))
 end
